c9e25c8e90c24904fcb6afd4417aeda2e138988b
xiaoxu <xiaoxu>
2016-10-08 16:54:12 +0800

    修改

--- Shop/Application/Home/Controller/AddressController.class.php
+++ Shop/Application/Home/Controller/AddressController.class.php
@@ -18,23 +18,62 @@ class AddressController extends ApiController
     }
 
     /**
-     * 获取市
+     * 获取城市 \n
+     * URI : /home/address/getCity
+     * @param :
+     *     name   | type   | null | description
+     * -----------|--------|------|-------------
+     *   -------  | ------ | 不必填| ------
+     *
+     * @return
+     *    name  |  type   | description
+     * ---------|---------|----------------------
+     *   list   |  array  |  城市列表
+     *
+     * list :
+     *    name   |  type  | description
+     * ----------|--------|----------------------
+     *   city_id |  int   |  城市ID
+     * city_name | string |  城市名称
+     *
      */
     public function getCity()
     {
-        $list = M('area_china')->where(array('type' => 3))->field('id as city_id,name')->select();
-        $this->_returnData($list);
+        $list = M('area_china')->where(array('type' => 3))->field('id as city_id,name as city_name')->select();
+        $this->_returnData(['list' => $list]);
     }
 
+
     /**
-     * 获取县/区
+     * 获取县/区 \n
+     * URI : /home/address/getDistrict
+     * @param :
+     *     name   | type   | null | description
+     * -----------|--------|------|-------------
+     *  parameters| string | 必填 | 参数(json)
+     *
+     * parameters:
+     *   name   | type | null | description
+     * ---------|------|------|-------------
+     *  city_id |  int | 必填 | 城市ID
+     *
+     * @return
+     *    name  |  type   | description
+     * ---------|---------|----------------------
+     *   list   |  array  |  城市列表
+     *
+     * list :
+     *      name     |  type  | description
+     * --------------|--------|----------------------
+     *     city_id   |  int   |  县/区ID
+     * district_name | string |  县/区名称
+     *
      */
     public function getDistrict()
     {
         $parameters = $this->_createParameters();
-        //$city_id = $parameters['city_id'];
-        $list = M('area_china')->where(array('type' => 4, 'parent_id' => $parameters['city_id']))->field('id as district_id,name')->select();
-        $this->_returnData($list);
+        $list = M('area_china')->where(array('type' => 4, 'parent_id' => $parameters['city_id']))->field('id as district_id,name as district_name')->select();
+        $this->_returnData(['list' => $list]);
     }
 
 }
